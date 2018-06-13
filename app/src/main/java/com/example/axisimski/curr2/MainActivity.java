215783bcabb4632e029ca5f8ddd8ea389f55bacd
Alexander Simeonovski <33306491+axisimski@users.noreply.github.com>
2018-06-13 01:40:54 -0500

    //

--- app/src/main/java/com/example/axisimski/curr2/MainActivity.java
+++ app/src/main/java/com/example/axisimski/curr2/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.axisimski.curr2;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ListView listView;
+    List <String> list;
+    ListAdapter adapter;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView=findViewById(R.id.songList);
+        list=new ArrayList<>();
+
+        Field[] fields=R.raw.class.getFields();
+
+        for(int i=0;i<fields.length;i++){
+            list.add(fields[i].getName());
+        }
+
+
+        adapter=new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(adapter);
+
+
+
+        listView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
