560054628d0bcafedeb47f64154457a3a4923190
Alexander Simeonovski <33306491+axisimski@users.noreply.github.com>
2018-06-13 08:13:10 -0500

    Plays from list!

--- app/src/main/java/com/example/axisimski/curr2/PlayBackActivity.java
+++ app/src/main/java/com/example/axisimski/curr2/PlayBackActivity.java
@@ -1,10 +1,12 @@
 package com.example.axisimski.curr2;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 
@@ -27,12 +29,13 @@ public class PlayBackActivity extends AppCompatActivity {
         seekBar=findViewById(R.id.seekBar);
         playButton=findViewById(R.id.playButton);
 
-
+        //Prep Media Player, including URI info from intent
         mediaPlayer=new MediaPlayer();
         Uri uri=Uri.parse(getIntent().getStringExtra("URI"));
 
         try {
             mediaPlayer.setDataSource(getApplicationContext(), uri);
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
             if (mediaPlayer != null) {
                 mediaPlayer.prepare();
@@ -44,7 +47,91 @@ public class PlayBackActivity extends AppCompatActivity {
             mediaPlayer.start();
         }
 
+        seekBar.setMax(mediaPlayer.getDuration());
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                if(fromUser){
+                    mediaPlayer.seekTo(progress);
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                seekBar.setMax(mediaPlayer.getDuration());
+                playCycle();
+                mediaPlayer.start();
+            }
+        });
+
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                    handler.removeCallbacks(runnable);
+                }
+
+                else mediaPlayer.start();
+            }
+        });
+
 
     }
     //end of OnCreate();
-}
+
+
+
+    protected void onResume(){
+        super.onResume();
+        mediaPlayer.start();
+    }
+    /*protected void onPause(){
+        super.onPause();
+        mediaPlayer.stop();
+    }
+    protected void onDestroy(){
+        super.onDestroy();
+        mediaPlayer.release();
+        handler.removeCallbacks(runnable);
+    }*/
+
+
+    public void playCycle(){
+        seekBar.setProgress(mediaPlayer.getCurrentPosition());
+
+        if(mediaPlayer.isPlaying()){
+            runnable=new Runnable() {
+                @Override
+                public void run() {
+                    playCycle();
+                }
+            };
+            handler.postDelayed(runnable, 10);
+        }
+
+    }
+
+
+
+}//end class
