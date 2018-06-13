a67491146af898116c910f2ace20d3bc81db19ea
Alexander Simeonovski <33306491+axisimski@users.noreply.github.com>
2018-06-13 03:22:59 -0500

    //

--- app/src/main/java/com/example/axisimski/curr2/MainActivity.java
+++ app/src/main/java/com/example/axisimski/curr2/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.axisimski.curr2;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +16,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -46,11 +51,50 @@ public class MainActivity extends AppCompatActivity {
                       new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},MPR);
           }
 
-      }else //doSTUFF
+      }//else //doSTUFF
     }
 
 
-    public void getMusic(){}
+    public void getMusic(){
+        ContentResolver contentResolver=getContentResolver();
+        Uri songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor songCursor=  contentResolver.query(songUri, null, null, null, null);
+
+
+        if(songCursor!=null&&songCursor.moveToFirst()){
+
+            int songTitle=songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+
+
+
+            do{
+                String currentTitle=songCursor.getString(songTitle);
+
+            }while(songCursor.moveToNext());
+
+        }
+
+
+    }
+
+    @Override
+
+    public void onRequestPermissionResult(int requestCode, String[]permissions, int[]grantResults){
+
+        switch (requestCode){
+            case MPR:{
+                if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    if(ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(this,"PMG!", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        }
+
+    }
+
 
 
 
